f00ab98d5fefa4b85762619aedc9b31df49a529c
Andreas Tsarouchas <andreastsar@gmail.com>
2022-04-06 21:49:30 +0300

    end of lesson 1 Java

---
+++ Java_on_class/Lesson_1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 250;
+        int y = 250;
+        float vat = 1.24f;
+        int sum = x + y;
+
+        // Variables can't start with number f.e. 2x
+        int $k = 15;
+        System.out.println("The sum is : " + (sum * vat));
+    }
+
+}
